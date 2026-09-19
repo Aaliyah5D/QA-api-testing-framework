777a1e884c784e4a85c1a66bd28ec5e23f6a99a4
Aaliyah Dube <aadubjhb025@student.wethinkcode.co.za>
2026-09-19 14:26:31 +0200

    test: add positive and negative GET scenarios

--- src/test/java/tests/GetPostsTest.java
+++ src/test/java/tests/GetPostsTest.java
@@ -1,21 +1,45 @@
 package tests;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class GetPostsTest {
     
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+    
     @Test
     public void shouldGetAllPosts() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-        
         given()
             .when()
             .get("/posts")
             .then()
             .statusCode(200)
             .body("size()", greaterThan(0));
+    }
+
+    @Test
+    void shouldGetSinglePost() {
+        given()
+            .when()
+            .get("/posts/1")
+            .then()
+            .statusCode(200)
+            .body("id", equalTo(1))
+            .body("userId", equalTo(1));
+    }
+
+    @Test
+    void shouldReturnNotFoundForInvalidPost() {
+        given()
+            .when()
+            .get("/posts/999999")
+            .then()
+            .statusCode(404);
     }
 }
